8a6811ee95896c2707ecb54e499f9ba12ef777b6
AlvaroNrs <alvaronrs@hotmail.com>
2023-03-25 17:38:26 -0300

    Inicio do Sistema de Transição de Cenas

--- Project Capivara/Assets/Scripts/Player/CharacterInteractController.cs
+++ Project Capivara/Assets/Scripts/Player/CharacterInteractController.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class CharacterInteractController : MonoBehaviour
 {
-    private CharacterController character;
+    private PlayerMovement playerMovementController;
     private Rigidbody2D rgbd2d;
-    [SerializeField] private float offSetDistance = 1F;
+    [SerializeField] private float offsetDistance = 1F;
     [SerializeField] private float sizeOfInteractableArea = 1.2F;
-
+    private Character character;
+    
     void Awake()
     {
-        character = GetComponent<CharacterController>();
+        playerMovementController = GetComponent<PlayerMovement>();
         rgbd2d = GetComponent<Rigidbody2D>();
+        character = GetComponent<Character>();
     }
 
     private void Update()
@@ -23,8 +26,20 @@ public class CharacterInteractController : MonoBehaviour
         }
     }
 
-    private void Interact()
+    public void Interact()
     {
-        //Vector2 position = rgbd2d.position + offSetDistance;
+        Vector2 position = rgbd2d.position + playerMovementController.lastMotionVector * offsetDistance;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(position, sizeOfInteractableArea);
+
+        foreach (Collider2D c in colliders)
+        {
+            Interactable hit = c.GetComponent<Interactable>();
+            if (hit != null)
+            {
+                hit.Interact(character);
+                break;
+            }
+        }
     }
 }
---
+++ Project Capivara/Assets/Scripts/SceneTransition/Transition.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum TransitionType
+{
+    Warp,
+    Scene
+}
+;
+public class Transition : MonoBehaviour
+{
+    [SerializeField] private TransitionType transitionType;
+    [SerializeField] private string sceneNameToTransition;
+    
+    private Transform destination;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        destination = transform.GetChild(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InitiateTransition(Transform toTransition)
+    {
+        switch (transitionType)
+        {
+            case TransitionType.Warp:
+                toTransition.position = new Vector3(
+                    destination.position.x,
+                    destination.position.y,
+                    toTransition.position.z);
+                break;
+            
+            case TransitionType.Scene:
+                SceneManager.LoadScene(sceneNameToTransition);
+                break;
+        }
+        
+    }
+}
