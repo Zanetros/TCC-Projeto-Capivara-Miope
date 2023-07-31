c17d7ccd5baa4a5e575469856abb2eb53833c1fe
AlvaroNrs <alvaronrs@hotmail.com>
2023-07-31 11:49:54 -0300

    Save de Crops Ajustado a se Verificar

--- Project Capivara/Assets/Scripts/Plantação/TilemapCropsManager.cs
+++ Project Capivara/Assets/Scripts/Plantação/TilemapCropsManager.cs
@@ -16,8 +16,10 @@ public class TilemapCropsManager : TimeAgent
 
     public CropsContainer container;
     public CropsContainer allCropsInGame;
+    public List<SpriteRenderer> cropsRenders;
+    public List<int> c;
+    private int d = 0;
 
-    
     //Função de Exemplo
     public void TestarComparacaoDeCrop(int id)
     {
@@ -36,6 +38,7 @@ public class TilemapCropsManager : TimeAgent
     
     public void Start()
     {
+        d = 0;
         GameManager.instance.GetComponent<CropsManager>().tileCropsManager = this;
         targetTilemap = GetComponent<Tilemap>();
         onTimeTick += Tick;
@@ -71,7 +74,7 @@ public class TilemapCropsManager : TimeAgent
             }
 
             cropsTile.growTimer += 1;
-
+            /*
             if (cropsTile.growTimer >= cropsTile.crop.growthStageTime[cropsTile.growthStage])
             {
                 cropsTile.renderer.gameObject.SetActive(true);
@@ -80,6 +83,20 @@ public class TilemapCropsManager : TimeAgent
                 if (cropsTile.growthStage + 1 < cropsTile.crop.sprites.Count)
                 {
                     cropsTile.growthStage += 1;
+                }
+            }*/
+        }
+
+        for (int i = 0; i < cropsRenders.Count; i++)
+        {
+            if (container.crops[i].crop != null && 
+                container.crops[i].growTimer >= container.crops[i].crop.growthStageTime[container.crops[i].growthStage])
+            {
+                cropsRenders[i].gameObject.SetActive(true);
+                cropsRenders[i].sprite = container.crops[i].crop.sprites[container.crops[i].growthStage];
+                if (container.crops[i].growthStage + 1 < container.crops[i].crop.sprites.Count)
+                {
+                    container.crops[i].growthStage += 1;
                 }
             }
         }
@@ -117,16 +134,20 @@ public class TilemapCropsManager : TimeAgent
             GameObject go = Instantiate(cropsSpritePrefab, transform);
             go.transform.position = targetTilemap.CellToWorld(cropsTile.position);
             go.transform.position -= Vector3.forward * 0.01f;
+            cropsRenders.Add(go.GetComponent<SpriteRenderer>());
             cropsTile.renderer = go.GetComponent<SpriteRenderer>();
+            c.Add(d);
         }
         bool growing = cropsTile.crop != null && cropsTile.growTimer >= cropsTile.crop.growthStageTime[0];
-
+        
         cropsTile.renderer.gameObject.SetActive(growing);
 
         if (growing == true)
         {
             cropsTile.renderer.sprite = cropsTile.crop.sprites[cropsTile.growthStage-1];
+            cropsRenders[d].sprite = cropsTile.crop.sprites[cropsTile.growthStage-1];
         }      
+        d++;
     }
     
     private void CreatePlowedTile(Vector3Int position)
