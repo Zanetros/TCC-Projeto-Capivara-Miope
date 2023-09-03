e343fb66e8cda47a09402a18a82c80c5acc56793
Zanetros <pztkawall@gmail.com>
2023-09-02 21:01:47 -0300

    Adição de novos itens

--- Project Capivara/Assets/Scripts/Coleta/EatAction.cs
+++ Project Capivara/Assets/Scripts/Coleta/EatAction.cs
@@ -8,7 +8,7 @@ public class EatAction : ToolActions
     public override void OnItemUsed(Item usedItem, ItemContainer inventory)
     {
         inventory.Remove(usedItem);
-        GameManager.instance.playerStamina.GiveStamina(5);
+        GameManager.instance.playerStamina.GiveStamina(10);
         Debug.Log("comeu");
     }
 }
